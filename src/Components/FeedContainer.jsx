1f751808de0915b18c1c8f58b43373a045c2833b
Moutasim02 <moutasim9997@gmail.com>
2023-08-21 10:56:14 +0400

    Added fixed feeds before introducing APIs to the page

--- src/Components/FeedContainer.jsx
+++ src/Components/FeedContainer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "../style.css";
 import {NewsBlock} from "./NewsBlock";
 
+// Show all feeds from the endpoint
 export const FeedContainer = () => {
+
     return (
         <div>
             <div className="titleContainer">
@@ -11,16 +13,15 @@ export const FeedContainer = () => {
             <div>
                 <div className="feedContainer">
                     <div className="newsList">
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
-                        <NewsBlock></NewsBlock>
+                        <NewsBlock title="How modular can your monolith go? Part 2 - a first look at how subdomains collaborate"
+                                   link="https://microservices.io//post/architecture/2023/08/20/how-modular-can-your-monolith-go-part-2.html" ></NewsBlock>
+
+                        <NewsBlock title="Exploring Burp Suite’s Features: A Detailed Overview"
+                                   link="https://infosecwriteups.com/exploring-burp-suites-features-a-detailed-overview-2bacf809c6f8" ></NewsBlock>
+
+                        <NewsBlock title="Applying Design Principles In React"
+                                   link="https://blog.openreplay.com/applying-design-principles-in-react/" ></NewsBlock>
+
                     </div>
                 </div>
             </div>
--- src/Components/NewsBlock.jsx
+++ src/Components/NewsBlock.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "../style.css";
 
-export const NewsBlock = () => {
+export const NewsBlock = (params) => {
+    const domain = extractDomain(params.link);
+
     return (
-        <div className="newsBlock"></div>
+        <div className="newsBlock">
+                <a href={params.link}>{params.title + " - " + domain}</a>
+        </div>
     );
 };
+
+function extractDomain(url) {
+    let domain = "";
+    const regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im;
+    const match = url.match(regex);
+    if (match && match.length > 1) {
+        domain = match[1];
+    }
+    return domain;
+}
